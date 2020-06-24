bc7959be41806f425c6c4f5341e9b8ac3eff4b0b
sandy777deep <66700051+sandy777deep@users.noreply.github.com>
2020-06-23 20:46:57 -0500

    ICP5

---
+++ Web/Documents/ICP5/sandy/src/app/app.component.ts
@@ -0,0 +1,64 @@
+import { Component } from '@angular/core';
+
+
+
+@Component({
+
+  selector: 'app-root',
+
+  templateUrl: './app.component.html',
+
+  styleUrls: ['./app.component.css']
+
+})
+
+export class AppComponent {
+
+  title = 'sandy';
+
+
+
+  newTodo: string;
+
+  todos: any;
+
+  todoObj: any;
+
+  constructor() {
+
+    this.newTodo = '';
+
+    this.todos = [];
+
+  }
+
+
+
+  addTodo(event) {
+
+    this.todoObj =
+      {
+
+      newTodo: this.newTodo,
+      };
+
+    this.todos.push(this.todoObj);
+
+    this.newTodo = '';
+
+    event.preventDefault();
+
+  }
+
+
+
+  deleteTodo(index) {
+
+    this.todos.splice(index, 1);
+
+  }
+
+}
+
+
+
